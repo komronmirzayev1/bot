d56ae90ff0dfd3cb12ab233dadfbb0e50cc62e78
komronmirzayev1 <126227324+komronmirzayev1@users.noreply.github.com>
2023-12-26 17:00:41 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,52 @@
+import wikipedia
+import asyncio
+import logging
+import sys
+from os import getenv
+
+from aiogram import Bot, Dispatcher, Router, types
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from aiogram.utils.markdown import hbold
+
+# Bot token can be obtained via https://t.me/BotFather
+TOKEN = "6842164296:AAFBi2jR1fKnyBA_eqhBNHj_JkWbS3NhUHs"
+wikipedia.set_lang('uz')
+
+bot=Bot(token=TOKEN)
+# All handlers should be attached to the Router (or Dispatcher)
+dp = Dispatcher(bot=bot)
+
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    """
+    This handler receives messages with `/start` command
+    """
+    # Most event objects have aliases for API methods that can be called in events' context
+    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
+    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
+    # method automatically or call API method directly via
+    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
+    await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
+
+
+@dp.message()
+async def sendWiki(message: types.Message) -> None:
+    try:
+        respond = wikipedia.summary(message.text)
+        await message.answer(respond)
+    except:
+        await message.answer("bu mavzuga oid maqola topilmadi")
+
+async def main() -> None:
+    # Initialize Bot instance with a default parse mode which will be passed to all API calls
+    bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
+    # And the run events dispatching
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
